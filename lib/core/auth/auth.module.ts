08a0515c4a59947c6321189eacb74c77cae5ccdf
Mikolaj Serwicki <mikolaj.serwicki@hyland.com>
2022-07-11 13:27:41 +0200

    faet: add aith related providers inside auth module

--- lib/core/auth/auth.module.ts
+++ lib/core/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { StorageService } from '@alfresco/adf-core/storage';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core';
 import { AuthConfig, OAuthModule, OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
+import { authConfigFactory, AuthConfigService } from '../auth-factories/auth-config.service';
+import { OIDCAuthenticationService } from '../auth-factories/oidc-authentication.service';
+import { AuthGuard } from '../services/auth-guard.service';
+import { AuthBearerInterceptor } from './auth-bearer.interceptor';
 import { AuthRoutingModule } from './auth-routing.module';
-import { AuthModuleConfig, AUTH_MODULE_CONFIG } from './auth.module.token';
+import { AuthModuleConfig, AUTH_CONFIG, AUTH_MODULE_CONFIG } from './auth.module.token';
 import { AuthService } from './auth.service';
+import { BaseAuthenticationService } from './base-authentication.service';
+import { OidcAuthGuard } from './oidc-auth.guard';
 import { RedirectAuthService } from './redirect-auth.service';
 import { AuthenticationConfirmationComponent } from './view/authentication-confirmation/authentication-confirmation.component';
 
@@ -31,6 +38,14 @@ const defaultConfig: AuthModuleConfig = {
       useFactory: loginFactory,
       deps: [OAuthService, OAuthStorage, AUTH_MODULE_CONFIG],
       multi: true
+    },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthBearerInterceptor, multi: true },
+    { provide: AuthGuard, useClass: OidcAuthGuard },
+    { provide: BaseAuthenticationService, useClass: OIDCAuthenticationService },
+    {
+        provide: AUTH_CONFIG,
+        useFactory: authConfigFactory,
+        deps: [AuthConfigService]
     }
   ]
 })
