a98899b61be4b4a16e104f3706d232c7b05c716f
Mikolaj Serwicki <mikolaj.serwicki@hyland.com>
2022-08-30 12:27:36 +0000

    feat: auth add module

--- lib/core/auth/src/lib/auth.module.ts
+++ lib/core/auth/src/lib/auth.module.ts
@@ -1,7 +1,46 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { StorageService } from '@alfresco/adf-core/storage';
+import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core';
+import { AuthConfig, OAuthModule, OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthModuleConfig, AUTH_CONFIG, AUTH_MODULE_CONFIG } from './auth.module.token';
+import { AuthService } from './auth.service';
+import { RedirectAuthService } from './redirect-auth.service';
+import { AuthenticationConfirmationComponent } from './view/authentication-confirmation/authentication-confirmation.component';
+
+export function loginFactory(oAuthService: OAuthService, storage: OAuthStorage, config: AuthConfig) {
+    const service = new RedirectAuthService(oAuthService, storage, config);
+    return () => service.init();
+};
+
+const defaultConfig: AuthModuleConfig = {
+    useHash: false
+};
 
 @NgModule({
-    imports: [CommonModule],
+  declarations: [AuthenticationConfirmationComponent],
+  imports: [
+    AuthRoutingModule,
+    OAuthModule.forRoot()
+  ],
+  providers: [
+    RedirectAuthService,
+    { provide: AuthService, useExisting: RedirectAuthService },
+    { provide: OAuthStorage, useExisting: StorageService },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loginFactory,
+      deps: [OAuthService, OAuthStorage, AUTH_CONFIG],
+      multi: true
+    }
+  ]
 })
-export class AuthModule {}
+export class AuthModule {
+  static forRoot(config: AuthModuleConfig = defaultConfig): ModuleWithProviders<AuthModule> {
+    return {
+      ngModule: AuthModule,
+        providers: [
+            { provide: AUTH_MODULE_CONFIG, useValue: config }
+        ]
+    };
+  }
+}
