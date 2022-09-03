c74d4824acddfc9680cd61086b9f0afecb0a2acd
Mikolaj Serwicki <mikolaj.serwicki@gmail.com>
2022-09-03 13:29:43 +0000

    feat: add auth module

---
+++ lib/core/auth/src/index.ts
@@ -0,0 +1,9 @@
+export * from './lib/auth-bearer.interceptor';
+export * from './lib/auth-routing.module';
+export * from './lib/auth.module';
+export * from './lib/auth.module.token';
+export * from './lib/auth.service';
+export * from './lib/oidc-auth.guard';
+export * from './lib/redirect-auth.service';
+export * from './lib/view/authentication-confirmation/authentication-confirmation.component';
+
---
+++ lib/core/auth/src/lib/auth.module.ts
@@ -0,0 +1,30 @@
+import { StorageService } from '@alfresco/adf-core/storage';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { AuthConfig, OAuthModule, OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
+import { AuthRoutingModule } from './auth-routing.module';
+import { AUTH_CONFIG } from './auth.module.token';
+import { AuthService } from './auth.service';
+import { RedirectAuthService } from './redirect-auth.service';
+import { AuthenticationConfirmationComponent } from './view/authentication-confirmation/authentication-confirmation.component';
+
+export function loginFactory(oAuthService: OAuthService, storage: OAuthStorage, config: AuthConfig) {
+    const service = new RedirectAuthService(oAuthService, storage, config);
+    return () => service.init();
+}
+
+@NgModule({
+    declarations: [AuthenticationConfirmationComponent],
+    imports: [AuthRoutingModule, OAuthModule.forRoot()],
+    providers: [
+        RedirectAuthService,
+        { provide: AuthService, useExisting: RedirectAuthService },
+        { provide: OAuthStorage, useExisting: StorageService },
+        {
+            provide: APP_INITIALIZER,
+            useFactory: loginFactory,
+            deps: [OAuthService, OAuthStorage, AUTH_CONFIG],
+            multi: true
+        }
+    ]
+})
+export class AuthModule {}
