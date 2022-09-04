91d72627f2c7821cb91e7255b12da2a42b7dafa7
Mikolaj Serwicki <mikolaj.serwicki@gmail.com>
2022-09-04 14:57:19 +0000

    feat: clean up

--- lib/core/auth/src/lib/auth.module.ts
+++ lib/core/auth/src/lib/auth.module.ts
@@ -1,4 +1,3 @@
-import { StorageService } from '@alfresco/adf-core/storage';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { AuthConfig, OAuthModule, OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
 import { AuthRoutingModule } from './auth-routing.module';
@@ -18,7 +17,6 @@ export function loginFactory(oAuthService: OAuthService, storage: OAuthStorage, 
     providers: [
         RedirectAuthService,
         { provide: AuthService, useExisting: RedirectAuthService },
-        { provide: OAuthStorage, useExisting: StorageService },
         {
             provide: APP_INITIALIZER,
             useFactory: loginFactory,
